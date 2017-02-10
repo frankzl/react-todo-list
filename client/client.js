257712a03c26dd67522c838b45bda6e5a24fd135
Frank Lu <frankz.lu@gmail.com>
2017-02-10 08:43:29 +0100

    this is a react app now

--- client/client.js
+++ client/client.js
@@ -1,1 +1,9 @@
+import React from 'react'
+import {render, method1} from 'react-dom'
+import App from '../components/App'
 
+render(
+    //define the compassing component
+    // Dom element we want to mount it to
+    <App/>, document.getElementById('app')
+)
