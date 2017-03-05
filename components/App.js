2273b66a7314e632cd1a9f47908e594df98bf3e0
Frank Lu <frankz.lu@gmail.com>
2017-03-05 15:59:27 +0100

    added text view

--- components/App.js
+++ components/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import TextInput from './TextInput'
 
 class App extends Component {
 
-  render() {
-    return <div>This is definitely a hot (module reloading) React app now!</div>
-  }
+    render() {
+        return ( 
+            <div className="app">
+                <div>This is definitely a hot (module reloading) React app now!</div>
+                <div>This is another line</div>
+                <TextInput/>
+            </div>
+        )
+    }
 
 }
 
